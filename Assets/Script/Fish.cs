c250e8a10bc8829ab57190ffe5bbcbfe37e63add
Pablo-Thieriot <pablo.thieriot@gmail.com>
2026-02-25 15:48:06 +0100

    fix : lag

--- Assets/Script/Fish.cs
+++ Assets/Script/Fish.cs
@@ -11,6 +11,12 @@ public class Fish : MonoBehaviour
     [SerializeField] private string permanentEffect;
     [SerializeField] private int isPermanent;
     [SerializeField] private int speed;
+
+    [SerializeField] private bool isFind;
+    [SerializeField] private bool isHooked; 
+    [SerializeField] private Material baseMaterial;
+    [SerializeField] private Material HookedMaterial;
+
     /*private GameObject fishObject;
 
     private void Awake()
@@ -18,7 +24,18 @@ public class Fish : MonoBehaviour
         fishObject = GetComponent<GameObject>();
     }*/
 
-        
+    private void Update()
+    {
+        Physics.Raycast(Vector3.zero, Vector3.up);
+    }
+    void OnDrawGizmos()
+    {
+        Ray ray = cam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        Gizmos.color = Color.red;
+        Gizmos.DrawRay(ray.origin, ray.direction * 50f);
+    }
+
+
     public string Name
     {
         get { return name; }
