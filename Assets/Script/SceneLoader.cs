095bcf28a6434ca1eeb297ae58f3a7aeac1353c2
Pablo-Thieriot <pablo.thieriot@gmail.com>
2026-02-25 14:05:08 +0100

    Feat : all scene

---
+++ Assets/Script/SceneLoader.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    private void Start()
+    {
+        if (!SceneManager.GetSceneByName("World_Environment").isLoaded)
+            SceneManager.LoadScene("World_Environment", LoadSceneMode.Additive);
+
+        if (!SceneManager.GetSceneByName("World_Gameplay").isLoaded)
+            SceneManager.LoadScene("World_Gameplay", LoadSceneMode.Additive);
+
+        if (!SceneManager.GetSceneByName("World_UI").isLoaded)
+            SceneManager.LoadScene("World_UI", LoadSceneMode.Additive);
+    }
+}
