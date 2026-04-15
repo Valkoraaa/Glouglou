ce248278f364c35323ba462718dc34314b84cabf
Pablo-Thieriot <pablo.thieriot@gmail.com>
2026-04-15 15:26:15 +0200

    feat : UI collection

---
+++ Assets/Script/Collection/BookGenerator.cs
@@ -0,0 +1,34 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BookGenerator : MonoBehaviour
+{
+    [SerializeField] private List<FishData> allFishes;
+    [SerializeField] private Transform gridLayoutGroup;
+    [SerializeField] private GameObject slotPrefab;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void OnEnable()
+    {
+        RefreshBook();
+    }
+
+    public void RefreshBook()
+    {
+        // On nettoie la grille pour éviter les doublons
+        foreach (Transform child in gridLayoutGroup)
+        {
+            Destroy(child.gameObject);
+        }
+
+        // On génère la liste à jour
+        foreach (FishData fish in allFishes)
+        {
+            GameObject newSlot = Instantiate(slotPrefab, gridLayoutGroup);
+            Collection_Slot slotScript = newSlot.GetComponent<Collection_Slot>();
+            bool caught = FishingBookManager.Instance.IsFishCaught(fish.id);
+            slotScript.SetUp(fish, caught);
+        }
+    }
+
+}
