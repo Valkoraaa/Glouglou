c65b5bf6a126e9709fac2d3a154e68f7003e0de6
Pablo-Thieriot <189406844+Pablo-Thieriot@users.noreply.github.com>
2026-06-19 16:24:36 +0200

    feat : icon

--- Assets/Script/Ui/UiGestion.cs
+++ Assets/Script/Ui/UiGestion.cs
@@ -12,11 +12,21 @@ public class UiGestion : MonoBehaviour
     private int lastFishCaught = -1;
     private int lastNumberOfFails = -1;
 
+    [Header("Icône débuff")]
+    [SerializeField] private Image debuffIcon;
+    [SerializeField] private Sprite drunkSprite;
+    [SerializeField] private Sprite drugSprite;
+    [SerializeField] private Sprite sickSprite;
+    [SerializeField] private Sprite sleepSprite;
+    [SerializeField] private Sprite noStrengthSprite;
+    [SerializeField] private Sprite depressionSprite;
+
     void Start()
     {
         Instance = this;
         UpdateFishCountText();
         InitFailSlider();
+        debuffIcon.gameObject.SetActive(false);
     }
 
     void Update()
@@ -30,12 +40,26 @@ public class UiGestion : MonoBehaviour
         {
             UpdateFailSlider();
         }
-    }
+        UpdateDebuffIcon();
 
+    }
     private void UpdateFishCountText()
     {
         lastFishCaught = DayManager.Instance.fishCaught;
         fishCountText.text = lastFishCaught.ToString();
+    }
+
+    private void UpdateDebuffIcon()
+    {
+        bool[] effects = EffectManager.Instance.effects;
+
+        if (effects[0]) { debuffIcon.sprite = drunkSprite; debuffIcon.gameObject.SetActive(true); }
+        else if (effects[1]) { debuffIcon.sprite = drugSprite; debuffIcon.gameObject.SetActive(true); }
+        else if (effects[2]) { debuffIcon.sprite = sickSprite; debuffIcon.gameObject.SetActive(true); }
+        else if (effects[3]) { debuffIcon.sprite = sleepSprite; debuffIcon.gameObject.SetActive(true); }
+        else if (effects[4]) { debuffIcon.sprite = noStrengthSprite; debuffIcon.gameObject.SetActive(true); }
+        else if (effects[5]) { debuffIcon.sprite = depressionSprite; debuffIcon.gameObject.SetActive(true); }
+        else { debuffIcon.gameObject.SetActive(false); }
     }
 
     private void InitFailSlider()
