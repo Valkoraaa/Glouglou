3cb4865f43475b288b51d987d7a131b3c72a6b44
Pablo-Thieriot <189406844+Pablo-Thieriot@users.noreply.github.com>
2026-06-19 19:15:23 +0200

    fix : collection UI

--- Assets/Script/Collection/Collection_Slot.cs
+++ Assets/Script/Collection/Collection_Slot.cs
@@ -5,14 +5,18 @@ public class Collection_Slot : MonoBehaviour
 {
     [Header("UI References")]
     [SerializeField] private Image displayIcon;
-
     [Header("Settings")]
     [SerializeField] private Sprite unknownSprite;
 
     public void SetUp(FishData data, bool isCaught, CollectionGestion manager)
     {
-        GetComponent<Button>().onClick.AddListener(() => manager.DisplayFishInfo(data));
         displayIcon.sprite = isCaught ? data.icon : unknownSprite;
         displayIcon.color = Color.white;
+
+        Button btn = GetComponent<Button>();
+        if (isCaught)
+        {
+            btn.onClick.AddListener(() => manager.DisplayFishInfo(data));
+        }
     }
 }
